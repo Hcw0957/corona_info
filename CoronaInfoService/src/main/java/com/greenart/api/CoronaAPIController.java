516e9bc3a6bc835afab4999a5ed52f986bd6beeb
Hcw0957 <kcm0957@naver.com>
2021-08-10 18:12:33 +0900

    시도 피싱 및 데이터 작업

--- CoronaInfoService/src/main/java/com/greenart/api/CoronaAPIController.java
+++ CoronaInfoService/src/main/java/com/greenart/api/CoronaAPIController.java
@@ -2,7 +2,7 @@ package com.greenart.api;
 
 import java.io.IOException;
 import java.net.URLEncoder;
-import java.text.SimpleDateFormat;asdsadasdasd
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -12,6 +12,7 @@ import javax.xml.parsers.DocumentBuilderFactory;
 
 import com.greenart.service.CoronaInfoService;
 import com.greenart.vo.CoronaInfoVO;
+import com.greenart.vo.CoronaSidoInfoVO;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -104,6 +105,103 @@ public class CoronaAPIController {
         return resultMap;
     }
     public static String getTagValue(String tag, Element elem){
+        NodeList nlList = elem.getElementsByTagName(tag).item(0).getChildNodes();
+        if(nlList == null) return null;
+        Node node = (Node)nlList.item(0);
+        if(node == null) return null;
+        return node.getNodeValue();
+        // public static String getTagValue(String tag, Element elem){
+        //     NodeList nlList = null;
+        //     Node nValue= null;
+        //     try{
+        //         nlList = elem.getElementsByTagName(tag).item(0).getChildNodes();
+        //         nValue = (Node) nlList.item(0);
+        //     }
+        //     catch(NullPointerException ne){
+        //         System.out.println("npe");
+        //         return "0";
+        //     }
+        //     if(nValue == null) return "0";
+        //     return nValue.getNodeValue();
+        // }
+
+        @GetMapping("/api/corona")
+    public Map<String, Object> getCoronaSidoInfo(@RequestParam String startDt, @RequestParam String endDt) throws Exception{
+        Map<String, Object> resultMap =  new LinkedHashMap<String, Object>();
+        StringBuilder urlBuilder = new StringBuilder("http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19SidoInfStateJson"); /*URL*/
+        urlBuilder.append("?" + URLEncoder.encode("serviceKey","UTF-8") + "=3CID6KRU4kjF4jvHanoFBLwycg6Htt86aVfgEOgBmAecshZIcO5EC9UM9FhVGwAX2Zf%2B%2FrxgsJeUfled1zNS0w%3D%3D"); /*Service Key*/
+        urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*페이지번호*/
+        urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode("1000", "UTF-8")); /*한 페이지 결과 수*/
+        urlBuilder.append("&" + URLEncoder.encode("startCreateDt","UTF-8") + "=" + URLEncoder.encode(startDt, "UTF-8")); /*검색할 생성일 범위의 시작*/
+        urlBuilder.append("&" + URLEncoder.encode("endCreateDt","UTF-8") + "=" + URLEncoder.encode(endDt, "UTF-8")); /*검색할 생성일 범위의 종료*/
+
+        DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+        Document doc = docBuilder.parse(urlBuilder.toString());
+
+        doc.getDocumentElement().normalize();      
+        NodeList nList = doc.getElementsByTagName("item");
+        if(nList.getLength() <= 0){
+            resultMap.put("status", false);
+            resultMap.put("message", "데이터가 없습니다.");
+            return resultMap;
+
+        }
+        for(int i=0; i<nList.getLength(); i++){
+            Node node = nList.item(i);
+            Element elem = (Element) node;
+            // // System.out.println(getTagValue("accDefRate", elem));    //누적 확진자 수
+            // System.out.println(getTagValue("accExamCnt", elem));    //누적 검사수
+            // System.out.println(getTagValue("accExamCompCnt", elem));    //누적 검사 완료 수
+            // System.out.println(getTagValue("careCnt", elem));       //치료 중
+            // System.out.println(getTagValue("clearCnt", elem));      //격리 해제
+            // // System.out.println(getTagValue("createDt", elem));      //등록일
+            // System.out.println(getTagValue("deathCnt", elem));      //사망자 수
+            // System.out.println(getTagValue("decideCnt", elem));     //확진
+            // System.out.println(getTagValue("examCnt", elem));       //검사진행 수
+            // System.out.println(getTagValue("resutlNegCnt", elem));  // 음성
+            // // System.out.println(getTagValue("seq", elem));           //데이터 번호
+            // // System.out.println(getTagValue("stateDt", elem));       //기준일
+            // System.out.println(getTagValue("stateTime", elem));     //기준시간
+            // // System.out.println(getTagValue("updateDt", elem));      //수정일
+            // System.out.println("==============================================================================");
+
+            CoronaSidoInfoVO vo = new CoronasidoInfoVO();
+            vo.setAccExamCnt(Integer.parseInt(getTagValue2("seq", elem)));
+            vo.setAccExamCompCnt (Integer.parseInt(getTagValue2("createDt", elem)));
+            vo.setCareCnt(Integer.parseInt(getTagValue2("deathCnt", elem)));
+            vo.setClearCnt(Integer.parseInt(getTagValue2("defCnt", elem)));
+            vo.setDeathCnt(Integer.parseInt(getTagValue2("gubun", elem)));
+            vo.setDecideCnt(Integer.parseInt(getTagValue2("incDec", elem)));
+            vo.setExamCnt(Integer.parseInt(getTagValue2("isolClearCnt", elem)));
+            vo.setResultNegCnt(Integer.parseInt(getTagValue2("isolIngCnt", elem)));
+            vo.setResultNegCnt(Integer.parseInt(getTagValue2("localOccCnt", elem)));
+            vo.setResultNegCnt(Integer.parseInt(getTagValue2("overFlowCnt", elem)));
+
+            // String to Date (시간)
+            Date dt = new Date();
+            SimpleDateFormat dtFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            dt = dtFormat.parse(getTagValue("createDt", elem));
+            vo.setStateTime(dt);
+            // System.out.println(vo);
+            service.insertCoronaInfo(vo);
+        }
+        resultMap.put("status", true);
+            resultMap.put("message", "데이터가 입력되었습니다.");
+            return resultMap;
+    }
+    @GetMapping("/api/coronasidoInfo/{date}")
+    public Map<String, Object> getCoronaSidoInfo(@PathVariable String date) {
+        Map<String, Object> resultMap = new LinkedHashMap<String, Object>();
+        CoronaInfoVO data = null;
+        if(date.equals("today")){
+            data = service.selectTodayCoronaInfo();
+        }
+        resultMap.put("status", true);
+        resultMap.put("data", data);
+        return resultMap;
+    }
+    public static String getTagValue2(String tag, Element elem){
         NodeList nlList = elem.getElementsByTagName(tag).item(0).getChildNodes();
         if(nlList == null) return null;
         Node node = (Node)nlList.item(0);
--- CoronaInfoService/src/main/java/com/greenart/service/CoronaInfoService.java
+++ CoronaInfoService/src/main/java/com/greenart/service/CoronaInfoService.java
@@ -7,6 +7,7 @@ import java.util.Date;
 
 import com.greenart.mapper.CoronaInfoMapper;
 import com.greenart.vo.CoronaInfoVO;
+import com.greenart.vo.CoronaSidoInfoVO;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -19,6 +20,45 @@ public class CoronaInfoService {
         mapper.insertCoronaInfo(vo);
     }
     public CoronaInfoVO selectTodayCoronaInfo(){
+        // Calendar start = Calendar.getInstance();
+        // Calendar end = Calendar.getInstance();
+        
+        // start.set(Calendar.HOUR_OF_DAY, 10);
+        // start.set(Calendar.MINUTE, 30);
+        // start.set(Calendar.SECOND, 0);
+        
+        //2021-08-11 01:00:00 - 이전시간 
+        //2021-08-11 10:30:00 - 세팅값
+        //2021-08-11 14:30:00 - 이후시간
+
+        //현재 시간이 세팅값보다 이전시간 일때, 전날 데이터를 뽑아주고
+        //현재 시간이 세팅값보다 이후시간 이라면, 오늘 데이터를 뽑아준다.
+
+        Date now = new Date();
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+        String date = formatter.format(now);
+
+        CoronaInfoVO data = mapper.selectCoronaInfoByDate(date);
+
+        Integer accExamCnt = data.getAccExamCnt();
+        Integer decideCnt = data.getDecideCnt();
+
+        DecimalFormat dFormatter = new DecimalFormat("###,###");
+        String strAccExamCnt = dFormatter.format(accExamCnt);
+        String strDecideCnt = dFormatter.format(decideCnt);
+        
+        data.setStrAccExamCnt(strAccExamCnt);
+        data.setStrDecideCnt(strDecideCnt);
+
+        return data;
+
+        //10시 30분 - 10시 29분(이전 날의 데이터).. 
+
+    }
+    public void insertCoronaSidoInfo(CoronaSidoInfoVO vo){
+        mapper.insertCoronaSidoInfo(vo);
+    }
+    public CoronaInfoVO selectTodayCoronaSidoInfo(){
         // Calendar start = Calendar.getInstance();
         // Calendar end = Calendar.getInstance();
         
